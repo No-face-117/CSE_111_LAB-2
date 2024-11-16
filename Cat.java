708ccfce2139e916d5a609efcf6c1d212c26f65a
No-face-117 <sayedmahamudalif@gmail.com>
2024-11-16 22:42:44 +0600

    Cat object

---
+++ Cat.java
@@ -0,0 +1,13 @@
+// CSE-111 LAB-2 UG TASK-1     []
+// Cat object
+
+public class Cat {
+    public String color = "White";
+    public String action = "sitting";
+
+    public void printCat(){
+        System.out.println(color + " cat is " +  action);
+    }
+}
+ 
+
